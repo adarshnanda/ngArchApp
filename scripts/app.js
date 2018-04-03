0e40787bb9ada0718d8a6e0d586638bdf53210b0
Adarsh Nanda <adarsh.nanda@t-mobile.com>
2018-04-03 20:37:11 +0530

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ngArchApp
+ * @description
+ * # ngArchApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ngArchApp', [
+    'ui.router'
+  ])
+  .config(function($stateProvider,$locationProvider){
+    $stateProvider
+    .state('main',{
+      url:'^/main',
+      templateUrl: 'scripts/views/main.html',
+      controller: 'mainCtrl'
+    })
+    .state('about',{
+      url:'^/about',
+      templateUrl: 'scripts/views/about.html',
+      controller: 'AboutCtrl'
+    });
+$locationProvider.html5Mode(true);
+    $locationProvider.hashPrefix('!');
+  });
+  /*.config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });*/
+
